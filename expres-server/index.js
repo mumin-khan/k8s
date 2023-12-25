d0acd420834d203f92fb9de4b1577e0f20ed1fa6
mumin-khan <muminkhan2000@gmail.com>
2023-12-25 08:43:21 +0530

    basic networking ingress

--- expres-server/index.js
+++ expres-server/index.js
@@ -1,8 +1,15 @@
 import express from 'express'
+import path from 'path'
 const app =express()
 
 const port = process.env.PORT || 3000;
+;
+app.use(express.static('./'));
 
+app.get('/', function (req, res)
+{
+  res.send('<h1>Hello, this is your HTML response!</h1>');
+})
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
--- logOutput/index.js
+++ logOutput/index.js
@@ -1,3 +1,9 @@
+import express from 'express'
+const app =express()
+
+const port = process.env.PORT || 3000;
+;
+
 function generateRandomString(length) {
     const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
     let randomString = '';
@@ -12,8 +18,18 @@ function generateRandomString(length) {
   
   // Example: Generate a random string of length 10
   const randomString = generateRandomString(10);
-  console.log(randomString)
+  let currentDate = new Date(Date.now()).toISOString()
     setInterval(() => {
-       console.log(randomString, new Date(Date.now()).toISOString()) 
+       currentDate = new Date(Date.now()).toISOString()
+       console.log(randomString,currentDate ) 
     }, 5000);
-  
+
+
+    app.get('/status', function (req, res)
+    {
+      res.send(`string:${randomString}, tstamp:${currentDate}`);
+    })
+    // Start the server
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
