f92247a4bb3151d65d600c27ba8dc02544b3d734
mumin-khan <muminkhan2000@gmail.com>
2024-01-30 06:17:20 +0530

    ex-2.07

--- pingpong/index.js
+++ pingpong/index.js
@@ -1,41 +1,63 @@
 import express from 'express'
-// import path from 'path'
-// import fs from 'fs'
-// const directory = path.join('/', 'usr', 'src', 'app', 'files')
-// const filePath = path.join(directory, 'pingpong.txt')
+import pg from 'pg-promise'
 
-// const fileAlreadyExists = async () => new Promise(res => {
-//     fs.stat(filePath, (err, stats) => {
-//       if (err || !stats) return res(false)
-//       return res(true)
-//     })
-//   })
-  
-//   const findAFile = async () => {
-//     if (await fileAlreadyExists()) return
-//     await new Promise(res => fs.mkdir(directory,{recursive:true},(err) => console.log("error",err)))
-//   }
+const pgp = pg()
+const db = pgp(`postgres://${process.env.USERNAME}:${process.env.PASSWORD}@postgres-svc:5432/postgres`)
 
-// findAFile()
+
+
 const app = express()
+const createTableQuery = `
+  CREATE TABLE IF NOT EXISTS website_visits ( website_name VARCHAR(255), ping_count INT DEFAULT 0);
+`;
+
 
 let counter = 0 ;
 const port = process.env.PORT || 3000;
 
-app.get('/', function (req, res)
+app.get('/', async function (req, res)
 {
+  db.none(createTableQuery)
+  .then(() => {
+    console.log('Table created or already exists');
+  })
+  .catch((error) => {
+    console.error('Error creating table:', error);
+  })
+  let counter = 0
+  try{
+
+  counter = await db.one("SELECT ping_count FROM website_visits WHERE website_name = 'pingpong'")
+  console.log(counter)
+  counter = counter.ping_count;
+  }
+  catch(error)
+  {
+    console.log(error)
+  }
   counter+=1;
 //   fs.writeFile(filePath,counter, (writeErr) => {
 //     if (writeErr) {
 //       console.error('Error writing to the file:', writeErr);
 //     }
 //  } )
+await db.none("UPDATE website_visits SET ping_count = ping_count+1 WHERE website_name = 'pingpong'")
   res.send(`pong ${counter}`);
 
 })
 
-app.get('/count',function(req,res)
+app.get('/count',async function(req,res)
 {
+  let counter =0;
+  try{
+    counter =  await db.one("SELECT ping_count FROM website_visits WHERE website_name = 'pingpong'")
+    console.log(counter)
+  counter = counter.ping_count;
+  }
+  catch(error)
+  {
+    console.log(error)
+  }
   res.send(`${counter}`)
 })
 
